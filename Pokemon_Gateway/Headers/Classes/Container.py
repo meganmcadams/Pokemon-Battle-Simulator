ab90e009de19a8ec59213328d8faa87d45454d75
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-07-09 16:22:21 -0500

    clean up classes

--- Pokemon_Gateway/Headers/Classes/Container.py
+++ Pokemon_Gateway/Headers/Classes/Container.py
@@ -1,26 +1,33 @@
-class Container(object):
-    def __init__(self, list):
-        self.list = list  # is a dictionary with keys as index 0
+class Container(dict):
+    def __init__(self, dict_):
+        super().__init__(dict_)
+        self.list = self
 
-    def add(self, id, item):
-        self.list[id] = item
-        # add pokemon to master list
+    def add(self, pid, trainer):
+        self[pid] = trainer
+        # add trainer to master list
 
-    def remove(self, id):
-        del self.list[id]
-        # remove pokemon from master list
+    def remove(self, pid):
+        del self[pid]
+        # remove trainer from master list
 
-    def replace(self, id, item):
-        self.list[id] = item
-        # replace item with new item
+    def replace(self, pid, trainer):
+        self[pid] = trainer
+        # replace trainer with new trainer
+
+    def get_pid(self, name):
+        return self.get_id('Name', name)
+
+    def get_name(self, tid):
+        return self.get_field_content(tid, 'Name')
 
     def get_id(self, field_name, field_content):
-        for item in self.list:
+        for item in self:
             if item[field_name] == field_content:
                 return item['ID']
         return -1
         # find id of item with field (field_name) matching field_content
 
     def get_field_content(self, id, field_content):
-        return self.list[id][field_content]
+        return self[id][field_content]
         # find name of item's "field_name"
