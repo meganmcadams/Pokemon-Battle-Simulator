7ee058ccf7d46e45872dddb34598276811d38c6c
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-08-22 19:16:18 -0500

    flesh out Trainer class

--- Pokemon_Gateway/Headers/Classes/Trainer.py
+++ Pokemon_Gateway/Headers/Classes/Trainer.py
@@ -1,9 +1,21 @@
+from Headers.Classes.Pokemon import Pokemon
+from Headers.Classes.SavedPokemon import SavedPokemon
+
 TRAINERS = {}
 
 
 class Trainer:
     def __init__(self):
-        pass
+        self.id: int = 0
+        self.player: str = ""
+        self.name: str = ""
+        self.pokedex_count: list[Pokemon] = []
+        self.pokedex: str = ""
+        self.pokemon_count: int = 0
+        self.pokemon: list[SavedPokemon] = []
+        self.money: int = 0
+        self.items: list[str] = []
+
 
     @staticmethod
     def get_all():
@@ -13,3 +25,7 @@ class Trainer:
     def register(trainer):
         # todo: finish
         TRAINERS[trainer.id] = trainer
+
+    @staticmethod
+    def get_trainer(id_: int) -> 'Trainer':
+        return TRAINERS[id_]
