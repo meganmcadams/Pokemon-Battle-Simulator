d047bb2bee411b7c9b87ddbf443edb0479cea467
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-08-23 02:21:22 -0500

    Shop class

--- Pokemon_Gateway/Headers/Classes/Shop.py
+++ Pokemon_Gateway/Headers/Classes/Shop.py
@@ -1,13 +1,63 @@
-# from Container import Container
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from Headers import Item
 
-SHOPS = {}
+SHOPS: dict[int, 'Shop'] = {}
 
 
-class Shop:
+class Shop():
     def __init__(self):
-        super().__init__(self)
-        self.list = self
+        super().__init__()
+        self.id: int = 0
+        self.name: str = ""
+        self.location: str = ""
+        self.items: list[Item] = []
+        self.berries: int = 0
+        self.general_items: int = 0
+        self.machine: int = 0
+        self.medicine: int = 0
+        self.pokeballs: int = 0
 
     @staticmethod
-    def get_all():
+    def register(shop_: dict) -> None:
+        shop = Shop()
+        shop.id = shop_["ID"]
+        shop.name = shop_["Name"]
+        shop.location = shop_["Location"]
+        shop.items = [Item.get_item(int(i)) for i in str(shop_["Items"]).rstrip(',').split(',')]
+        shop.berries = shop_["Berries"]
+        shop.general_items = shop_["General items"]
+        shop.machine = shop_["Machine"]
+        shop.medicine = shop_["Medicine"]
+        shop.pokeballs = shop_["Pokeballs"]
+        SHOPS[shop.id] = shop
+
+    @staticmethod
+    def as_dicts():
+        retval = {}
+        for key, value in SHOPS.items():
+            retval[key] = {
+                "ID": value.id,
+                "Name": value.name,
+                "Location": value.location,
+                "Items": ','.join([str(item.id) for item in value.items]),
+                "Berries": value.berries,
+                "General items": value.general_items,
+                "Machine": value.machine,
+                "Medicine": value.medicine,
+                "Pokeballs": value.pokeballs
+            }
+        return retval
+
+    @staticmethod
+    def get_shop(id_: int) -> 'Shop':
+        return SHOPS[id_]
+
+    @staticmethod
+    def get_all() -> dict[int, 'Shop']:
         return SHOPS
+
+    @staticmethod
+    def delete(id_: int) -> None:
+        del SHOPS[id_]
+    
