0f7d22e44f5910f9b3e8c66a872071838a1dbc92
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-08-27 16:54:50 -0500

    finish up trainer class

--- Pokemon_Gateway/Headers/Classes/Trainer.py
+++ Pokemon_Gateway/Headers/Classes/Trainer.py
@@ -1,6 +1,6 @@
 import typing
 
-from Headers import Pokemon
+from Headers import Pokemon, Item
 from Headers import SavedPokemon
 
 TRAINERS = {}
@@ -18,15 +18,32 @@ class Trainer:
         self.money: int = 0
         self.items: list[str] = []
 
-
     @staticmethod
     def get_all():
         return TRAINERS
 
     @staticmethod
-    def register(trainer):
+    def register(trainer_: typing.Union[dict, 'Trainer']) -> None:
+        if isinstance(trainer_, Trainer):
+            TRAINERS[trainer_.id] = trainer_
+            return
         # todo: finish
-        TRAINERS[trainer["ID"]] = trainer
+        trainer = Trainer()
+        trainer.id = trainer_["ID"]
+        trainer.player = trainer_["Player"]
+        trainer.name = trainer_["Name"]
+        if trainer.pokedex:
+            trainer.pokedex = [Pokemon.get_pokemon(int(i)) for i in str(trainer_["Pokedex"]).rstrip(',').split(',')]
+        trainer.pokedex_count = trainer_["Pokedex Count"]
+        trainer.pokemon_count = trainer_["Pokemon Count"]
+        # todo: trainer["Pokemon"] is a list of dex entries, not SavedPokemon entries
+        if trainer.pokemon:
+            trainer.pokemon = [Pokemon.get_pokemon(int(i)) for i in str(trainer_["Pokemon"]).rstrip(',').split(',')]
+            # trainer.pokemon = [SavedPokemon.get_pokemon(int(i)) for i in str(trainer_["Pokemon"]).rstrip(',').split(',')]
+        trainer.money = trainer_["Money"]
+        if trainer.items:
+            trainer.items = [Item.get_item(int(i)) for i in str(trainer_["Items"]).rstrip(',').split(',')]
+        TRAINERS[trainer.id] = trainer
 
     @staticmethod
     def get_trainer(id_: int) -> 'Trainer':
@@ -41,13 +58,21 @@ class Trainer:
                 "Player": value.player,
                 "Name": value.name,
                 "Pokedex Count": value.pokedex_count,
-                "Pokedex": value.pokedex,
+                "Pokedex": ','.join([str(i.id) for i in value.pokedex]),
                 "Pokemon Count": value.pokemon_count,
-                "Pokemon": value.pokemon,
+                "Pokemon": ','.join([str(i.id) for i in value.pokemon]),
                 "Money": value.money,
-                "Items": ','.join(value.items)
+                "Items": ','.join([str(i.id) for i in value.items])
             }
+        return retval
 
     @staticmethod
     def delete(id_: int) -> None:
         del TRAINERS[id_]
+
+    @staticmethod
+    def list_trainers() -> None:
+        name_width = max([len(value.name) for value in TRAINERS.values()]) + 2
+        print(f"{'ID':<5}{'Name':<{name_width}}{'Player':<20}")
+        for key, value in TRAINERS.items():
+            print(f"{key:<5}{value.name:<{name_width}}{value.player:<20}")
